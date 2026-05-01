c70a939519c2309e6d44cfc8e65f087e554031e5
HudutiEnes <hudutienes95@gmail.com>
2026-05-01 19:32:09 +0200

    feat: decouple route paths and align with API versioning

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getTasks,
-  getTasksbyID,
-  createTask,
-  updateTask,
-  deleteTask,
-  patchTask,
+    getTasks,
+    getTasksbyID,
+    createTask,
+    updateTask,
+    deleteTask,
+    patchTask,
 } = require("../controllers/tasksControllers");
 
-router.get("/api/tasks", getTasks);
-router.get("/api/tasks/:id", getTasksbyID);
-router.post("/api/tasks", createTask);
-router.put("/api/tasks/:id", updateTask);
-router.delete("/api/tasks/:id", deleteTask);
-router.patch("/api/tasks/:id", patchTask);
+router.get("/", getTasks);
+router.get("/:id", getTasksbyID);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+router.patch("/:id", patchTask);
 
 module.exports = router;
