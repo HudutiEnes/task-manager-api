20b2908bfaa8c794d05083d1aeae71733ce18aa3
HudutiEnes <hudutienes95@gmail.com>
2026-05-08 20:47:07 +0200

    Added ssl and firewall service https to firewall script and updated nginx to listen on port 443

--- scripts/setup-firewall.sh
+++ scripts/setup-firewall.sh
@@ -2,13 +2,16 @@
 # DevOps Project Firewall setup
 
 # Creating the zone
-firewall-cmd --permanent --new-zone=devops-projects
+firewall-cmd --permanent --new-zone=devops-projects 2>/dev/null
 
 # Adding nginx/api ports
 firewall-cmd --permanent --zone=devops-projects --add-port=8080/tcp
-firewall-cmd --permanent --zone=devops-projects --add-port=3000/tcp
+firewall-cmd --permanent --zone=devops-projects --add-service=https
 
 # Reload firewall so it applies the change
 firewall-cmd --reload
+
+sudo firewall-cmd --permanent --zone=devops-projects --change-interface=wlp0s20f3
+sudo firewall-cmd --reload
 
 echo "Network security applied to devops-projects zone."
